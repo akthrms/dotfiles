25d0dfde025b9ee2493e5228a4dd555cd2634817
akthrms <akita@rockwave.co.jp>
2019-12-02 19:43:38 +0900

    add

---
+++ config.fish
@@ -0,0 +1,40 @@
+# alias
+alias ll "lsd -al"
+alias cat "bat"
+alias vim "nvim"
+alias rosrun "rlwrap ros run"
+
+# dockertoolbox
+alias fig "docker-compose"
+alias exechttpd "docker exec -it httpd /bin/bash"
+function dtb
+  /Applications/Docker/Docker\ Quickstart\ Terminal.app/Contents/Resources/Scripts/start.sh $argv
+end
+
+# golang
+set -U GOPATH $HOME/go
+set -U fish_user_paths $GOPATH/bin
+
+# nodejs
+set -U fish_user_paths $HOME/.nodebrew/current/bin $fish_user_paths
+
+# flutter
+set -U fish_user_paths $HOME/my-flutter/flutter/bin $fish_user_paths
+
+# android
+set -U ANDROID_HOME /Users/akita/Library/Android/sdk
+set -U ANDROID_SDK_ROOT /Users/akita/Library/Android/sdk
+set -U ANDROID_AVD_HOME /Users/akita/.android/avd
+
+# pyenv
+set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths
+
+# .local
+set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+
+# rust
+set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
+set -U RUST_SRC_PATH $HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src
+
+# roswell
+set -U fish_user_paths $HOME/.roswell/bin $fish_user_paths
